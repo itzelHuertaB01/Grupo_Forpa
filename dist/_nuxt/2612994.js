fc8b35675163cc60fc9342bc582e74f478a22579
Antonio Pacheco <antonio6pache6@gmail.com>
2025-04-11 10:31:28 -0600

    pruebas con el dist

---
+++ dist/_nuxt/2612994.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[24],{641:function(n,e,t){"use strict";t.r(e);t(28),t(59);var o={mounted:function(){this.$router.replace("/preventive/Shopping_Pre")}},r=t(72),component=Object(r.a)(o,(function(){return(0,this._self._c)("div")}),[],!1,null,null,null);e.default=component.exports}}]);
